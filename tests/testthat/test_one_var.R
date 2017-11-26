83036844b48525825d8b9f2b1372ce3bc74df216
Bernie Gray <bfgray3@gmail.com>
2017-11-25 21:45:51 -0500

    add tests for one variable

---
+++ tests/testthat/test_one_var.R
@@ -0,0 +1,69 @@
+context("one_var")
+
+f1 <- function(n) {
+  if (n < 3) {
+    return(1)
+  } else {
+    Recall(n - 1) + Recall(n - 2)
+  }
+}
+
+r1 <- bigo(f = f1, n = 1:15)
+time1 <- r1[["runtimes"]]
+name1 <- r1[["function_name"]]
+runs1 <- r1[["num_runs"]]
+
+r2 <- bigo(f = function(n) if (n < 3) 1 else { Recall(n - 1) + Recall(n - 2) },
+           n = 1:15)
+time2 <- r2[["runtimes"]]
+name2 <- r2[["function_name"]]
+runs2 <- r2[["num_runs"]]
+
+test_that("Runtime tibble is right.", {
+
+  expect_is(time1, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(dim(time1), c(15, 2))
+  expect_equal(time1[["n"]], 1:15)
+  expect_identical(names(time1), c("n", "elapsed"))
+
+  expect_is(time2, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(dim(time2), c(15, 2))
+  expect_equal(time2[["n"]], 1:15)
+  expect_identical(names(time2), c("n", "elapsed"))
+
+})
+
+test_that("Number of runs is right.", {
+
+  expect_is(runs1, "numeric")
+  expect_equal(runs1, 1)
+
+  expect_is(runs2, "numeric")
+  expect_equal(runs2, 1)
+
+})
+
+test_that("Overall bigo object is right.", {
+
+  expect_is(r1, "bigo")
+  expect_equal(length(r1), 3)
+  expect_identical(names(r1), c("runtimes", "function_name", "num_runs"))
+
+  expect_is(r2, "bigo")
+  expect_equal(length(r2), 3)
+  expect_identical(names(r2), c("runtimes", "function_name", "num_runs"))
+
+})
+
+
+test_that("Function name is right.", {
+
+  expect_is(name1, "character")
+  expect_identical(name1, "f1")
+
+  expect_is(name2, "character")
+  expect_identical(name2, "anonymous")
+
+})
+
+###
